8fee12e7b5ad7ef12b7fb27ede7c2d10a19bfe14
Rohan Singh <rohans212004@gmail.com>
2024-12-05 12:50:31 +0530

    Book-gal

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
