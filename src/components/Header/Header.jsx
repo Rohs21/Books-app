b9156306662e48050f8e42bbef8b149d31c6909b
Rohan Singh <rohans212004@gmail.com>
2024-12-21 21:53:15 +0530

    Updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
+
 const Header = () => {
   return (
-    <div>
-      
+    <div className='holder'>
+        <Header className='header'>
+            <Navbar />
+            <div className='header-content flex flex-c text-center text-white'>
+                <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
+                <p className='header-text fs-18 fw-3'>"My Books App is an online library where users can explore a vast collection of books across various genres. It allows users to search for any book, view detailed information, and discover new favorites effortlessly. Built using React, the app delivers a fast, responsive, and interactive experience, making it a perfect platform for book lovers to dive into an extensive world of literature."</p>
+                <SearchForm />
+            </div>
+        </Header>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
+import { AppProvider } from './context.';
 import './index.css';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import BookDetails from './components/BookDetails/BookDetails';
-import BookList from './components/BookList/Booklist';
-
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <Routes>
-      <Route path = "/" element = {<Home />}>
-      <Route path = "about" element = {<About />} />
-      <Route path = "book" element = {<BookList />} />
-      <Route path = "/book/:id" element = {<BookDetails />} />
-      </Route>
-  </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
-
-reportWebVitals();
