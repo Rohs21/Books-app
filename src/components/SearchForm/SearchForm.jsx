0b6a79216fa22d94ea4b7e441fdcb7670c06e3a7
Rohan Singh <rohans212004@gmail.com>
2024-12-24 23:51:03 +0530

    changes

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,15 @@
+import React, {useRef, useEffect} from 'react';
+import {FaSearch} from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../../context.';
+import "./SearchForm.css";
+
+const SearchForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchForm
