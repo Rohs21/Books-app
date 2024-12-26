c3e1776d39614ef514c942fca46799f0dc8b6e65
Rohan Singh <rohans212004@gmail.com>
2024-12-27 02:07:44 +0530

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,14 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className='holder'>
-        <Header className='header'>
+        <header className='header'>
             <Navbar />
             <div className='header-content flex flex-c text-center text-white'>
                 <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-                <p className='header-text fs-18 fw-3'>"My Books App is an online library where users can explore a vast collection of books across various genres. It allows users to search for any book, view detailed information, and discover new favorites effortlessly. Built using React, the app delivers a fast, responsive, and interactive experience, making it a perfect platform for book lovers to dive into an extensive world of literature."</p>
+                <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
                 <SearchForm />
             </div>
-        </Header>
+        </header>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,5 +38,4 @@ const Navbar = () => {
   )
 }
 
-
 export default Navbar
