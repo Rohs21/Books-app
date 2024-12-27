501ad9df284725b19603f4a25167f881aa827df7
Rohan Singh <rohans212004@gmail.com>
2024-12-27 13:37:42 +0530

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
             <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
             <p className='fs-17'>Welcome to My Books
             "Discover a world of stories, knowledge, and inspiration. Search for your favorite books, explore new genres, and find everything you need to fuel your reading journey. Dive in and let the adventure begin!</p>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+            <p className='fs-17'>Made with love by~ Rohan Singh</p>
           </div>
         </div>
       </div>
