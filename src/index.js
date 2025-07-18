d4aaf7cbd3480185a87cdf677354d7b49ab3e7a3
Rohan_Singh <129981121+Rohs21@users.noreply.github.com>
2025-07-18 14:39:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AppProvider } from './context.';
 import './index.css';
 import Home from './pages/Home/Home';
 import About from "./pages/About/About";
-import BookList from "./components/BookList/BookList";
+import BookList from "./components/BookList/BookList.jsx";
 import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
