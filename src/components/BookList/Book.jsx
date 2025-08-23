6e8180969bb035175bf3884674e2cb8824bc9b56
Rohan Singh <rohans212004@gmail.com>
2025-08-24 02:09:50 +0530

    react changes

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./BookList.css";
+import "./Booklist.css";
 
 const Book = (book) => {
   return (
--- src/components/BookList/Booklist.jsx
+++ src/components/BookList/Booklist.jsx
@@ -3,7 +3,7 @@ import { useGlobalContext } from '../../context.';
 import Book from "../BookList/Book";
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
-import "./BookList.css";
+import "./Booklist.css";
 
 //https://covers.openlibrary.org/b/id/240727-S.jpg
 
